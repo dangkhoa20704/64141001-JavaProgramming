71c61dab3e531bdc1ea654bc9f9a6d1a57c0b49a
khoa373289 <khoatran2072004@gmail.com>
2025-02-13 11:12:38 +0700

    Ví dụ đầu tiên
    Demo

---
+++ HelloWorld/src/XinChao.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("EM LẦN ĐẦU HỌC JAVA, Xin chào thầy");
+	}
+
+}
